2619c5512c67e1ef88d9a81e5b6e3bbce303191c
KanashiiDev <18560400+KanashiiDev@users.noreply.github.com>
2026-09-20 03:45:16 +0300

    fix(activities/T/twitch.js): update URL pattern

--- activities/T/twitch.js
+++ activities/T/twitch.js
@@ -4,12 +4,12 @@ registerParser({
   authors: "kanashiiDev",
   authorsLinks: "https://github.com/KanashiiDev",
   title: "Twitch",
-  version: "1.0.2",
+  version: "1.0.3",
   description: "Twitch is an interactive livestreaming service for content spanning gaming, entertainment, sports, music, and more.",
   category: "platform",
   tags: ["streaming"],
   mode: "watch",
-  urlPatterns: [/\/.*/],
+  urlPatterns: [/\/.+/],
   fn: function () {
     const isOffline = document.querySelector(".channel-status-info--offline");
     const titleFallback = document.title !== "Twitch" && !isOffline ? document.title?.replace(" - Twitch", "") : "";
@@ -19,12 +19,13 @@ registerParser({
       titleFallback;
     const artist = getText("#live-channel-stream-information h1.tw-title");
     const image = getImage("#live-channel-stream-information")?.replace(/-\d{1,2}x\d{1,2}/, "-300x300") || "https://www.twitch.tv/favicon.ico";
-    const video = document.querySelector("video");
+    const video = document.querySelector("video:not([data-a-player-state='mini'] video)");
     const currentTime = Number.isFinite(video?.currentTime) && video.currentTime > 0 ? video.currentTime : 0;
     const duration = Number.isFinite(video?.duration) && video.duration > 0 ? video.duration : 0;
     const isPlaying = (!video?.paused && currentTime > 0) || Boolean(document.querySelector("[data-a-player-state='playing']"));
     const source = "Twitch";
     const songUrl = location.href;
     const timePassed = currentTime;
+    if (!video) clearActivity();
   },
 });
